9dbfc59c765391980d8379c750d0b4d607f0ed27
Han Fang <hfang8@crimson.ua.edu>
2019-11-25 20:49:32 -0600

    Update entities

---
+++ GroceryBamaSQLTest/Entities/PaymentMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace GroceryBama.Entities
+{
+    [DataContract]
+    public class PaymentMethod
+    {
+        public int PaymentMethodId { get; set; }
+        
+
+    }
+}
--- GroceryBamaSQLTest/Entities/User.cs
+++ GroceryBamaSQLTest/Entities/User.cs
@@ -8,14 +8,18 @@ namespace GroceryBama.Entities
     public class User
     {
         public string Username { get; set; }
-
         public string Firstname { get; set; }
-
         public string Lastname { get; set; }
-
         public string Role { get; set; }
-
         public string Token { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string StreetAddress { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public int GroceryId { get; set; }
 
     }
 
